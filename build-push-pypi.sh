4590f717d1581c8b84fed2253696180c85dd337b
satabios <max.satabiossathya@gmail.com>
2023-02-19 15:23:30 -0800

    Pushed Version 0.0.6

---
+++ build-push-pypi.sh
@@ -0,0 +1,22 @@
+
+package_build() {
+
+# sed -i  "8s/.*/version = \"$1\"/" pyproject.toml  
+# python3 setup.py sdist bdist_wheel 
+# twine upload "dist/tomoco-""$1""*"
+
+
+git add .
+git commit -m "Pushed Version $1"
+git push -u origin master
+}
+
+
+echo "Version to Build?"
+read version
+package_build "$version"
+
+
+
+
+# changing the 8th lines  [sed -i 'Ns/.*/replacement-line/' file.txt]
